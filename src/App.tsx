ca7b05f10d47574cbc83e970644cf06a4121fcce
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-16 10:58:12 +0100

    sass works

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ function App() {
 		<main>
 			<h1>Vite React Site</h1>
 			<p>Welcome to this site.</p>
+			<ul>
+				<li>one</li>
+				<li>two</li>
+				<li>three</li>
+			</ul>
 		</main>
 	);
 }
